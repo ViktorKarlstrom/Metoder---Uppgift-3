94395638336450c1e80212100cf049bba3b9673e
Viktor Karlström <46776740+ViktorKarlstrom@users.noreply.github.com>
2020-10-01 13:29:53 +0200

    La till uppgiftstexten

--- Metoder - Uppgift 3/Program.cs
+++ Metoder - Uppgift 3/Program.cs
@@ -8,6 +8,10 @@ namespace Metoder___Uppgift_3
 {
     class Program
     {
+        //3. Skapa ett program med en ny metod.Metoden skall ta emot två inparametrar av typen string
+        //och slå ihop dom till en sträng och returnera det nya värdet.Anropa den nya metoden från
+        //Main och skriv ut resultatet på skärmen.
+
         static void Main(string[] args)
         {
         }
